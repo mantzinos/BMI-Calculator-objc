e20d9229f66e7b60ac5b0741cfd19d5963925d2f
mantzinos <mantzinos@hotmail.com>
2025-01-27 00:33:00 +0200

    calculate

--- BMI-Calculator-objc/Controllers/ViewController.m
+++ BMI-Calculator-objc/Controllers/ViewController.m
@@ -8,6 +8,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *heightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *weightLabel;
+@property (weak, nonatomic) IBOutlet UISlider *heghtSlider;
+@property (weak, nonatomic) IBOutlet UISlider *weightSlider;
 
 @end
 
@@ -18,10 +22,17 @@
     // Do any additional setup after loading the view.
 }
 - (IBAction)heightSliderChanged:(UISlider *)sender {
-    NSLog(@"%f", sender.value);
+    self.heightLabel.text = [NSString stringWithFormat:@"%.2fm", sender.value];
 }
 - (IBAction)weightSliderChanged:(UISlider *)sender {
-    NSLog(@"%f", sender.value);
+    self.weightLabel.text = [NSString stringWithFormat:@"%.0fKg", sender.value];
+}
+- (IBAction)calculatePressed:(UIButton *)sender {
+    NSLog(@"%.2f", self.heghtSlider.value);
+    NSLog(@"%.0f", self.weightSlider.value);
+    
+    double bmi = self.weightSlider.value / pow(self.heghtSlider.value, 2);
+    NSLog(@"%.2f", bmi);
 }
 
 
